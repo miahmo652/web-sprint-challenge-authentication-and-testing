ea34a8f953092c17b1eabc6f9b059311d3d036d3
Mohammad <miahmo652@outlook.com>
2020-09-11 19:22:01 -0400

    update

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,31 @@
   before granting access to the next middleware/route handler
 */
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
-};
+const jwt = require("jsonwebtoken")
+
+function restrict(){
+  return async (req, res, next) =>{
+    const authError = {
+      message:"You shall not pass!"
+    };
+
+    try {
+      const token = req.cookies.token;
+      if(!token) {
+        return res.status(401).json(authError);
+      }
+      jwt.verify(token, process.env.JWT_SECRET, (err, decoded)=>{
+        if(err){
+          return res.status(401).json(authError)
+        };
+
+        next();
+      });
+
+    } catch(err){
+      next(err)
+    }
+  }
+}
+
+module.exports = restrict;
